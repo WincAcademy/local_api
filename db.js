7cf9bed224e46fc4dde215e501d837755a728e5e
Niels Bom <niels@nielsbom.com>
2021-01-31 15:24:41 +0100

    Make deletion simplere

--- db.js
+++ db.js
@@ -9,37 +9,38 @@ if (!db.todo) db.todo = [];
 const add = item => db.todo.push(item);
 
 const _findItem = id => {
-    const query = db.todo.where("_id").is(id);
-    if (query.length === 0 || query[0]._deleted)
-        return { ok: false, msg: "Unknown id" };
-    return { ok: true, msg: query[0] };
+  const query = db.todo.where("_id").is(id);
+  return query.length === 0
+    ? { ok: false, msg: "Unknown id" }
+    : { ok: true, msg: query[0] };
 };
 
 const get = id => {
-    const { ok, msg } = _findItem(id);
-    if (ok) return { ok: true, msg };
-    return { ok: false, msg: "Unknown id" };
+  const { ok, msg } = _findItem(id);
+  if (ok) return { ok: true, msg };
+  return { ok: false, msg: "Unknown id" };
 };
 
-const getAll = () => ({ ok: true, msg: db.todo.filter(i => !i._deleted) });
+const getAll = () => ({ ok: true, msg: db.todo });
 
 const update = (id, item) => {
-    const { ok, msg } = _findItem(id);
-    if (!ok) return { ok: false, msg: "Unknown id" };
-    Object.getOwnPropertyNames(item).forEach(prop => {
-        msg[prop] = item[prop];
-    });
-    return { ok: true, msg };
+  const { ok, msg } = _findItem(id);
+  if (!ok) return { ok: false, msg: "Unknown id" };
+  Object.getOwnPropertyNames(item).forEach(prop => {
+    msg[prop] = item[prop];
+  });
+  return { ok: true, msg };
 };
 
 const remove = id => {
-    const { ok, msg } = _findItem(id);
-    if (!ok) return { ok: false, msg: "Unknown id" };
+  const index = db.todo.findIndex(t => t._id === id);
 
-    // True deletion is not possible?
-    // https://github.com/small-tech/jsdb/issues/7
-    msg._deleted = true;
-    return { ok: true, msg: "Item deleted" };
+  if (index === -1) return { ok: false, msg: "Unknown id" };
+
+  // Slow, but good enough for our purposes.
+  // In-place update.
+  db.todo.splice(index, 1);
+  return { ok: true, msg: "Item deleted" };
 };
 
 module.exports = { add, get, getAll, update, remove };
